3e5756e288525fb2bac70b74a518ba2dbf5b42db
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2023-07-05 14:01:01 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,79 @@
+# flutter_esp32_ble_example
+flutterとesp32をBLEで通信するサンプルコードです。<br>
+サンプルの内容としてはFlutterとESP32で、BLEで接続を行い、<br>
+ESP32から定期的にデータを送信し、Flutter側で受信したデータを表示する実装と<br>
+Flutter側のフォームに入力した状態を、ESP32側に送信し、ESP32側で受信したデータを表示(Serial.print)する実装があります。<br>
+
+# プロジェクト構成
+esp_exampleにesp32のコード例が格納されています。<br>
+プロジェクトディレクトリ全体はflutterのプロジェクトになっています。
+
+# 使用技術
+- flutter
+- Arduino(esp32)
+- flutter_blue_plus
+
+# Bluetooth構成
+## セントラルとペリフェラル
+BLEにはセントラルとペリフェラルというものがあります。<br>
+セントラルとは、一般的にはスマートフォン側になることが多く、IoTデバイスなどと通信を行います。<br>
+ペリフェラルとは、一般的にはIoT側になることが多く、スマートフォンなどのセントラルと通信を行います。<br>
+また、セントラルはペリフェラルに対して、複数の接続を行うことができます。<br>
+
+## Service
+このプロジェクトでBLEの通信を行うためのサービスです。<br>
+Bluetoothのデバイス一覧では、このサービスのUUIDを持つデバイスが表示されます。<br>
+※このUUIDのサービスがないデバイスはこのFlutterアプリに対応していないと言えます<br>
+UUID: d6f19b1b-3b62-4d39-ad7d-ec2dddbeb0ee
+
+## Characteristic
+Characteristicとは、サービスの中で、データの送受信を行うためのものです。<br>
+このプロジェクトでは、ESP32のCharacteristicをFlutter側から読み取ったり、<br>
+書き込んだりしながらデータのやり取りを行います。
+一般的にはペリフェラル(EPS32)側が、Characteristicを作成し、<br>
+セントラル(Flutter)側が、Characteristicを読み取ったり、書き込んだりします。<br>
+
+### 3つのデータの送受信用Characteristic(補足)
+BLEにはread, write, notifyの3つのデータのやり取りの方法があります。<br>
+readは、Characteristicの値を読み取ることです。<br>
+writeは、Characteristicに値を書き込むことです。<br>
+notifyは特殊で、Characteristicの値が変更されたときに、<br>
+接続しているデバイス（今回はFlutter）などにそのことを通知するための仕組みです。<br>
+notifyがある理由としては、notifyを行わないと、定期的にreadを行う必要性が出てきてしまい、<br>
+省電力性が損なわれてしまうためです。<br>
+
+### 色の送信用Characteristic
+UUID: 30dfe503-7a09-4c5f-9a9f-352d773666d3<br>
+
+Flutter側からESP32に色の情報を送信するためのCharacteristicです。<br>
+色は3種類あって、赤,黄, 青の3色です。<br>
+BLE経由で文字列を送信するのは少し面倒なので、あらかじめ色に数字を割り当てます。<br>
+- 赤: 1
+- 黄: 2
+- 青: 3
+
+### ESP32からのデータ受信用Characteristic
+UUID: 78b5f08c-a793-4127-9a94-85a246056038<br>
+
+ESP32から任意の値(数値)を送信するCharacteristicです。<br>
+このCharacteristicを使って、ESP32からFlutter側にデータを送信します。<br>
+今回はESP32にセンサーなどをつないないでいないので、適当な値を送信していますが、<br>
+本来はESP32から取得したセンサーの値などを送信します。<br>
+また、このCharacteristicはESP32からFlutterに値が変化したことを伝えないといけないので、<br>
+notifyを有効にします。<br>
+
+### WiFiのSSID送信用のCharacteristic
+UUID: 54acff26-12f7-4502-a9b4-3f82a268df08
+
+### WiFiのパスワード送信用のCharacteristic
+UUID: bb11fb53-9d60-4f30-af5a-70485a572671
+
+# ファイル構成
+## lib/main.dart
+Flutterのメインのファイルです。<br>
+
+## lib/bluetooth_device_list.dart
+Bluetoothのデバイス一覧を取得し、表示する実装があるファイルです。<br>
+
+## lib/bluetooth_constants.dart
+BluetoothのUUIDなどの定数を定義しているファイルです。<br>
---
+++ lib/main.dart
@@ -0,0 +1,326 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_blue_plus/flutter_blue_plus.dart';
+import 'package:flutter_esp32_ble_example/bluetooth_constants.dart';
+import 'package:permission_handler/permission_handler.dart';
+// ignore: depend_on_referenced_packages
+import 'package:collection/collection.dart';
+
+
+void main() {
+  if (Platform.isAndroid) {
+    WidgetsFlutterBinding.ensureInitialized();
+    [
+      Permission.location,
+      Permission.storage,
+      Permission.bluetooth,
+      Permission.bluetoothConnect,
+      Permission.bluetoothScan
+    ].request().then((status) {
+      runApp(MyApp());
+    });
+  } else {
+    runApp(MyApp());
+  }
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Blue Example',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const FindDevicesScreen(),
+    );
+  }
+}
+
+class FindDevicesScreen extends StatefulWidget {
+  const FindDevicesScreen({super.key});
+
+  @override
+  State createState() => _FindDevicesScreenState();
+}
+
+class _FindDevicesScreenState extends State<FindDevicesScreen> {
+  FlutterBluePlus flutterBlue = FlutterBluePlus.instance;
+  List<BluetoothDevice> devices = [];
+
+  @override
+  void initState() {
+    super.initState();
+    startScan();
+  }
+
+  startScan() {
+    devices = [];
+
+    // Bluetoothデバイスをスキャンする
+    flutterBlue.startScan(timeout: const Duration(seconds: 4));
+
+    // スキャンした結果を受け取る
+    flutterBlue.scanResults.listen((results) {
+      setState(() {
+        for (ScanResult result in results) {
+          if (!devices.contains(result.device)) {
+            // 対応しているserviceが含まれているのかを確認する
+            final targetService = result.advertisementData.serviceUuids.contains(BluetoothConstants.serviceUuid);
+
+            // 対応しているserviceが含まれていれば、デバイス一覧の配列に追加する。
+            if (targetService) {
+              devices.add(result.device);
+            }
+          }
+        }
+      });
+
+    });
+
+
+    // 以前に接続したことのあるBluetoothを取得する
+    flutterBlue.connectedDevices.then((value) {
+      setState(() {
+        for (final result in value) {
+          if (!devices.contains(result)) {
+
+            devices.add(result);
+          }
+        }
+      });
+    });
+
+  }
+
+  @override
+  void dispose() {
+    flutterBlue.stopScan();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Find Devices'),
+      ),
+      body: ListView.builder(
+        itemCount: devices.length,
+        itemBuilder: (context, index) {
+          return BluetoothDeviceListTile(device: devices[index]);
+        },
+      ),
+    );
+  }
+}
+
+
+/// Bluetoothデバイスの一覧を表示するWidget
+/// 一つのWidgetに全てのコードを書いてしまうと、視認性（可読性）が低下してしまうので、
+/// 実装を切り分けるようにしている。
+class BluetoothDeviceListTile extends StatelessWidget {
+  const BluetoothDeviceListTile({super.key, required this.device});
+  final BluetoothDevice device;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(device.name),
+      subtitle: StreamBuilder(
+        stream: device.state,
+        initialData: BluetoothDeviceState.disconnected,
+        builder: (c, snapshot) {
+          switch(snapshot.data) {
+            case BluetoothDeviceState.connected:
+              return const Text('Connected');
+            case BluetoothDeviceState.connecting:
+              return const Text('Connecting');
+            case BluetoothDeviceState.disconnected:
+              return const Text('Disconnected');
+            case BluetoothDeviceState.disconnecting:
+              return const Text('Disconnecting');
+            default:
+              return const Text('Unknown');
+          }
+        },
+      ),
+      onTap: () async {
+        // タップされたら接続を開始する
+
+        // 最新の接続状況を取得して、それに応じて接続処理を行なって画面遷移を行う
+        device.state.first.then((value) async {
+          switch(value) {
+            case BluetoothDeviceState.connected:
+              break;
+            case BluetoothDeviceState.connecting:
+              break;
+            case BluetoothDeviceState.disconnected:
+              await device.connect();
+              break;
+            case BluetoothDeviceState.disconnecting:
+              await device.connect();
+              break;
+            default:
+              return;
+          }
+        });
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) => DeviceScreen(device: device),
+          ),
+        );
+      },
+    );
+  }
+}
+
+class DeviceScreen extends StatefulWidget {
+
+  const DeviceScreen({super.key, required this.device});
+  final BluetoothDevice device;
+
+  @override
+  State<StatefulWidget> createState() {
+    return DeviceScreenState();
+  }
+}
+class DeviceScreenState extends State<DeviceScreen> {
+  DeviceScreenState();
+  final _inputSsidController = TextEditingController();
+  final _inputPasswordController = TextEditingController();
+
+  Future<void> writeToCharacteristic(BluetoothCharacteristic characteristic, String value) async {
+    List<int> codeUnits = [];
+    for (int i = 0; i < value.length; i++) {
+      codeUnits.add(value.codeUnitAt(i));
+    }
+    await characteristic.write(codeUnits);
+  }
+  Future<BluetoothService?> findService() async {
+    // 現在のBluetoothの接続状態を取得する
+    final state = await widget.device.state.first;
+
+    // 接続状態が切断状態の場合は、接続処理を行う
+    if (state == BluetoothDeviceState.disconnected) {
+      await widget.device.connect();
+    }
+
+    // Bluetoothのサービスを取得する
+    final targetServices = await widget.device.discoverServices();
+
+    // 対応しているサービスを探す
+    return targetServices.firstWhereOrNull((element) {
+      return element.uuid.toString() == BluetoothConstants.serviceUuid;
+    });
+  }
+  /// デバイスにwifiの情報を送信する
+  void sendWifiInformation() async {
+    // 入力されたssidとpasswordの情報を取得する
+    final ssid = _inputSsidController.text;
+    final password = _inputPasswordController.text;
+    final service = await findService();
+
+    // wifiのssidを送信(書き込む)ためのcharacteristicを取得する
+    final wifiSsidCharacteristic = service?.characteristics.firstWhereOrNull((element) {
+      return element.uuid.toString() == BluetoothConstants.wifiSsidCharacteristicUuid;
+    });
+
+    // wifiのpasswordを送信(書き込む)ためのcharacteristicを取得する
+    final wifiPasswordCharacteristic = service?.characteristics.firstWhereOrNull((element) {
+      return element.uuid.toString() == BluetoothConstants.wifiPasswordCharacteristicUuid;
+    });
+
+    // wifiとpasswordのcharacteristicが取得できていれば、書き込みを行う
+    if (wifiSsidCharacteristic != null && wifiPasswordCharacteristic != null) {
+      await writeToCharacteristic(wifiSsidCharacteristic, ssid);
+      await writeToCharacteristic(wifiPasswordCharacteristic, password);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.device.name),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              ConnectionStatus(device: widget.device),
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                padding: const EdgeInsets.all(16),
+                width: double.infinity,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text("Wi-Fi設定", style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),),
+                    const SizedBox(height: 16),
+                    TextField(
+                      controller: _inputSsidController,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'SSID',
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    TextField(
+                      controller: _inputPasswordController,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'パスワード',
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        ElevatedButton(onPressed: sendWifiInformation, child: const Text("接続"))
+                      ],
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        )
+      )
+    );
+  }
+}
+
+class ConnectionStatus extends StatelessWidget {
+  final BluetoothDevice device;
+
+  const ConnectionStatus({super.key, required this.device});
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title:const  Text("接続状態"),
+      subtitle: StreamBuilder(
+        stream: device.state,
+        initialData: BluetoothDeviceState.disconnected,
+        builder: (c, snapshot) {
+          switch(snapshot.data) {
+            case BluetoothDeviceState.connected:
+              return const Text('Connected');
+            case BluetoothDeviceState.connecting:
+              return const Text('Connecting');
+            case BluetoothDeviceState.disconnected:
+              return const Text('Disconnected');
+            default:
+              return const Text('Unknown');
+          }
+        },
+      ),
+    );
+  }
+}
